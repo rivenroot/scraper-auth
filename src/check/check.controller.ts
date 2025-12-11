72da92704776d3a352057930277953bb790c8d30
Nikola Rakic <nikola.rakic@polysoft.rs>
2025-12-11 23:44:44 +0100

    aca

--- src/check/check.controller.ts
+++ src/check/check.controller.ts
@@ -12,6 +12,10 @@ export class CheckController {
       name: 'Nikola Test',
       uid: '197e03a57bd685b5',
     },
+    {
+      name: 'Aca Test',
+      uid: '49e40a3448fa0c00',
+    },
   ];
 
   @Get()
