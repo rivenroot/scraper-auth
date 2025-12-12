5348660ac591ea053ba4f44a8e31f9437b9444a5
Nikola Rakic <nikola.rakic@polysoft.rs>
2025-12-12 14:57:33 +0100

    asd

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useStaticAssets(path.join(__dirname, '..', 'public'));
 
   await app.listen(process.env.PORT ?? 3000);
 }
